168bb0853bd492672e7ec4a5073fb2e9527e1995
Noza M <cutenora05@gmail.com>
2020-11-01 13:49:44 -0600

    first commit

---
+++ src/main/java/WonderWoman.java
@@ -0,0 +1,16 @@
+public class WonderWoman {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hey!!! I am a Wonder Woman!");
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/test/java/SuperGirl.java
@@ -0,0 +1,18 @@
+public class SuperGirl {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hey!!! I am a Super Girl!");
+
+
+
+
+
+
+    }
+
+
+
+}
