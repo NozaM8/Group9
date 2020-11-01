58b8b12ca02059a70578bf7ef23b9dc22caabc54
sdet2020 <cybertek.b20@gmail.com>
2020-11-01 14:55:59 -0600

    created Interface so everyone can implement it the way he/she wants it!

---
+++ src/test/java/Akbarmirzo.java
@@ -0,0 +1,6 @@
+public class Akbarmirzo implements ProvideImplementation{
+
+    public void howDoYouLikeGitHubCollaboration(){
+        System.out.println("I am really enjoying our group 9 collaboration on GitHub!");
+    }
+}
---
+++ src/test/java/ProvideImplementation.java
@@ -0,0 +1,8 @@
+public interface ProvideImplementation {
+
+    /*
+    I'm creating this interface so that each one of us can implement it in our own classes
+    the way that we want to implement it!
+     */
+    void howDoYouLikeGitHubCollaboration();
+}
