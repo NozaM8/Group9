428e0063f2b98a9bed571103e9575c588e1471d4
tahsnerken47 <thsnerken47@gmail.com>
2020-11-01 16:36:54 -0500

    Github class

---
+++ src/test/java/LastGithubClass.java
@@ -0,0 +1,6 @@
+public class LastGithubClass {
+    public static void main(String[] args) {
+        System.out.println("I hope I will keep tht knowledge");
+    }
+
+}
--- src/test/java/TC001_taho.java
+++
@@ -1,2 +0,0 @@
-public class TC001_taho {
-}
